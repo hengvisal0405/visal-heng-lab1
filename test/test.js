c0305a579e578c5ca78dec72726fab858903fcd1
hengvisal0405 <imheangsocheata1900@gmail.com>
2025-11-07 21:04:07 +0700

    Add GitHub Actions CI workflow (Yarn)

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const app = require('../app');
 const { expect } = require('chai');
 
 describe('GET /', () => {
-  it('should return Hello, GitHub Actions!', async () => {
+  it('should return Hello, GitHub Action!', async () => {
     const res = await request(app).get('/');
     expect(res.status).to.equal(200);
     expect(res.text).to.equal('Hello, GitHub Actions!');
